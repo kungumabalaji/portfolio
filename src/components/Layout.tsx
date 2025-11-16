d1b25fb074abdc978a301b19a4590bed1e895f82
kunguma balaji Nagarajan <kungumabalaji1616@gmmail.com>
2025-11-16 04:13:42 +0000

    Updated layout and fixed About page UI

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,9 +7,9 @@ interface LayoutProps {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="min-h-screen bg-background">
+    <div className="min-h-screen bg-gray-100 text-gray-900">
       <Sidebar />
-      <main className="ml-72 min-h-screen">
+      <main className="ml-72 min-h-screen p-8">
         {children}
       </main>
     </div>
